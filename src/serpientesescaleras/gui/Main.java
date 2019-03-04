e1dbaabb09b3b700c2a8997367f03d64757b9f62
Carlos Contreras <cdca12mx@gmail.com>
2019-03-04 00:32:16 -0700

    Creación de proyecto

---
+++ src/serpientesescaleras/gui/Main.java
@@ -0,0 +1,13 @@
+package serpientesescaleras.gui;
+
+/**
+ *
+ * @author Carlos Contreras
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
